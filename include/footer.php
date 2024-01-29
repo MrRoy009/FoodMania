4f4c140543ad65bbde0ab8312801afb5e8a14eb6
MrRoy009 <153553141+monsta01010@users.noreply.github.com>
2024-01-29 12:59:08 +0530

    FoodMania-Web_Pirates
    Food Delivery Website

---
+++ include/footer.php
@@ -0,0 +1,31 @@
+      <footer class="footer">
+          <div class="container">
+
+
+              <div class="bottom-footer">
+                  <div class="row">
+                      <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                          <h5>Payment Options</h5>
+                          <ul>
+                              <li>
+                                  <a href="#"> <img src="images/razorpay-icon.png" style="width: 5rem; height:1.5rem; background-color:white;" alt="Razorpay"> </a>
+                              </li>
+                              
+                          </ul>
+                      </div>
+                      <div class="col-xs-12 col-sm-4 address color-gray">
+                          <h5>Address</h5>
+                          <p>NIT Agartala Jirania.</p>
+                          <h5>Phone: +91 XXXXX XXXXX</a></h5>
+                      </div>
+                      <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                          <h5>Copyright © 2024 - Web-Pirates - HACKNITA 2024</h5>
+                          
+                      </div>
+                  </div>
+              </div>
+
+          </div>
+      </footer>
+
+
